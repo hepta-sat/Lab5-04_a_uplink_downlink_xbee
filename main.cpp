740d53e25ba8c3fdaa6aaa43861d5862544c59b7
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-12-14 15:20:49 +0000

    v2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,44 @@
 #include "mbed.h"
+#include"HEPTA_EPS.h"
+#include"HEPTA_SENSOR.h"
+#include"HEPTA_COM.h"
 
-DigitalOut myled(LED1);
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_EPS eps(p16,p26);
+HEPTA_SENSOR sensor(p17,
+                  p28,p27,0x19,0x69,0x13,
+                  p13, p14,p25,p24);
+HEPTA_COM com(p9,p10,9600);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main()
+{
+    int i = 0,rcmd=0,cmdflag=0;
+    float bt,ax,ay,az;
+    com.printf("Count Up!\r\n");
+    while(1) {
+        com.printf("num = %d\r\n",i);
+        i++;
+        wait(1.0);
+        com.xbee_receive(&rcmd,&cmdflag);
+        pc.printf("rcmd=%d, cmdflag=%d\r\n",rcmd, cmdflag);
+        if (cmdflag == 1) {
+            if (rcmd == 'a') {
+                pc.printf("Command Get %d\r\n",rcmd);
+                com.printf("HEPTA Uplink OK\r\n");
+                pc.printf("===================\r\n");
+                pc.printf("Accel sensing Mode\r\n");
+                pc.printf("===================\r\n");
+                for(int ii = 0; ii < 10; ii++) {
+                    sensor.sen_acc(&ax,&ay,&az);
+                    eps.vol(&bt);
+                    com.printf("AX = %f\r\n",ax);
+                    com.printf("AY = %f\r\n",ay);
+                    com.printf("AZ = %f\r\n",az);
+                    com.printf("V = %f\r\n",bt);
+                    wait(0.5);
+                }
+            }
+            com.initialize();
+        }
+    }
 }
